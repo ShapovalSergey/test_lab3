24ac957f1d6cac1529b89541de59170758102b5f
ShapovalSergey <your_email@whatever.com>
2024-04-28 15:18:44 +0700

    Добавьте файлы проекта.

---
+++ test_lab3/main.cpp
@@ -0,0 +1,10 @@
+#include "main.h"
+
+using namespace testlab3;
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    Application::Run(gcnew main);
+    return 0;
+}
