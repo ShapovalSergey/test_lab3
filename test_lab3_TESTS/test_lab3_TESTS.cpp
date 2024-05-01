22e12e4a89569ff2cf52b1489c4c9708e30c93c2
ShapovalSergey <your_email@whatever.com>
2024-05-01 19:15:54 +0700

    Тестирование модуля генерации случайных чисел в диапазоне от 1 до 196, Которые не должны повторяться. Игра предполагает, что выдано будет 20 вопросов, поэтому тестирование проводится на 20 чисел. Тест не проходит

--- test_lab3_TESTS/test_lab3_TESTS.cpp
+++ test_lab3_TESTS/test_lab3_TESTS.cpp
@@ -13,7 +13,21 @@ namespace testlab3TESTS
 		
 		TEST_METHOD(TestMethod1)
 		{
+			newgamecapital ngc;
+			int n = 20;
+			vector <int> flags;
+			for (int i = 0; i < n; i++)
+			{
+				flags.push_back(ngc.getFlagId());
+			}
 
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 1; j+i < n; j++)
+				{
+					Assert::AreNotEqual(flags[i],flags[i+j]);
+				}
+			}
 		}
 	};
 }
