df50934d534411775dce49d56eba431e86bd2593
ShapovalSergey <your_email@whatever.com>
2024-05-01 21:48:12 +0700

    Реализация функции для нахождения подсказки по локации для страны

--- test_lab3/newgamecountry.cpp
+++ test_lab3/newgamecountry.cpp
@@ -32,5 +32,35 @@ string newgamecountry::getFlagAnswer(int flag)
 }
 string newgamecountry::getFlagHintLocation(int flag)
 {
-	return "";
+	char a[2];
+	string result;
+	FILE* p = fopen("D:\\projects\\test_lab3\\test_lab3\\hints.txt", "r");
+	fseek(p, (flag - 1) * 2, SEEK_SET);
+	fgets(a, 2, p);
+	fclose(p);
+	if (a[0] == '1')
+	{
+		result = "Европа";
+	}
+	else if (a[0] == '2')
+	{
+		result = "Северная америка";
+	}
+	else if (a[0] == '3')
+	{
+		result = "Южная америка";
+	}
+	else if (a[0] == '4')
+	{
+		result = "Азия";
+	}
+	else if (a[0] == '5')
+	{
+		result = "Африка";
+	}
+	else if (a[0] == '6')
+	{
+		result = "Океания";
+	}
+	return result;
 }
